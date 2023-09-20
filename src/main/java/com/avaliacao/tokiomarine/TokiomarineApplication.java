db2cfcf531107cd6096a8056a253bdf6f2174c9f
Vitor Stachanczyk <vstachancz@brasilseg.com.br>
2023-09-20 18:34:47 -0300

    First commit

---
+++ src/main/java/com/avaliacao/tokiomarine/TokiomarineApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.tokiomarine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokiomarineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokiomarineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avaliacao/tokiomarine/TokiomarineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.tokiomarine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokiomarineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
