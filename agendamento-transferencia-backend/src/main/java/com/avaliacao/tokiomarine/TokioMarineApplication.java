c7d91ba25ef87411cedecb800e5763446fc0c108
Vitor Stachanczyk <vstachancz@brasilseg.com.br>
2023-09-20 19:10:17 -0300

    Criacao de pacotes

--- agendamento-transferencia-backend/src/main/java/com/avaliacao/tokiomarine/TokioMarineApplication.java
+++ agendamento-transferencia-backend/src/main/java/com/avaliacao/tokiomarine/TokioMarineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TokiomarineApplication {
+public class TokioMarineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TokiomarineApplication.class, args);
+		SpringApplication.run(TokioMarineApplication.class, args);
 	}
 
 }
