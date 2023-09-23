7a65a25475a0ca7f5f24581c5a91aeffe620d378
Vitor Stachanczyk <vstachancz@brasilseg.com.br>
2023-09-23 11:48:52 -0300

    feat:adicao da anotacao @CrossOrigin para comunicacao entre backend e frontend

--- agendamento-transferencia-backend/src/main/java/com/avaliacao/tokiomarine/controller/AgendamentoTransferenciaController.java
+++ agendamento-transferencia-backend/src/main/java/com/avaliacao/tokiomarine/controller/AgendamentoTransferenciaController.java
@@ -13,39 +13,54 @@ import java.util.Optional;
 @RequestMapping("/api/agendamento-transferencia")
 public class AgendamentoTransferenciaController {
 
+    private static final String URL_ORIGIN = "http://localhost:5173";
+
+    private static final String REGRAS_TRANSFERENCIA_ERRO = "Erro ao salvar agendamento para transferência: Transferência inadequada às regras.";
+
     @Autowired
     private AgendamentoTransferenciaService agendamentoTransferenciaService;
 
 
+    @CrossOrigin(origins = URL_ORIGIN)
     @PostMapping("/add")
     public ResponseEntity addAgendamento(@RequestBody AgendamentoTransferenciaModel agendamentoTransferenciaModel){
         try {
-            return ResponseEntity
-                    .created(new URI("/api/agendamento-transferencia/" + agendamentoTransferenciaModel.getId()))
-                    .body(agendamentoTransferenciaService.addAgendamento(agendamentoTransferenciaModel));
+            var response = agendamentoTransferenciaService.addAgendamento(agendamentoTransferenciaModel);
+
+            if(response.getStatusCode().is2xxSuccessful()) {
+                return ResponseEntity
+                        .created(new URI("/api/agendamento-transferencia/" + agendamentoTransferenciaModel.getId()))
+                        .body(response);
+            }else{
+                return ResponseEntity.badRequest().body(REGRAS_TRANSFERENCIA_ERRO);
+            }
         }catch (Exception e){
             return ResponseEntity.badRequest().build();
         }
     }
 
+    @CrossOrigin(origins = URL_ORIGIN)
     @PutMapping("/edit")
     public ResponseEntity editAgendamentos(@RequestBody AgendamentoTransferenciaModel agendamentoTransferenciaModel){
         return ResponseEntity.ok(agendamentoTransferenciaService.update(agendamentoTransferenciaModel));
 
     }
 
+    @CrossOrigin(origins = URL_ORIGIN)
     @GetMapping("/{id}")
     public ResponseEntity getAgendamento(@PathVariable("id") Long id){
         return ResponseEntity.ok(agendamentoTransferenciaService.getAgendamentoById(id));
 
     }
 
+    @CrossOrigin(origins = URL_ORIGIN)
     @GetMapping("/agendamentos")
     public ResponseEntity getAgendamentos(){
         return ResponseEntity.ok(agendamentoTransferenciaService.getAgendamentos());
 
     }
 
+    @CrossOrigin(origins = URL_ORIGIN)
     @DeleteMapping("/{id}")
     public ResponseEntity deleteAgendamento(@PathVariable("id") Long id){
         Optional<AgendamentoTransferenciaModel> agendamentoTransferenciaModel = Optional.ofNullable(agendamentoTransferenciaService.getAgendamentoById(id));
